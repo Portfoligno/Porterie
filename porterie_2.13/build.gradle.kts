5df4ba6fa6ffa5131fb33e4b37decf7be27d56e7
Nicholas Yip <nicholasyip@obceit.cc>
2021-04-01 06:24:37 +0900

    Include Micrixalus as a transitive compile-time dependency

--- porterie_2.13/build.gradle.kts
+++ porterie_2.13/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 dependencies {
   val v = "2.13"
   implementation("org.scala-lang", "scala-library", "$v.5")
-  implementation("io.github.portfoligno.micrixalus", "scala_$v", "1.1.0")
-  implementation("io.github.portfoligno.micrixalus", "scala-predef_$v", "1.1.0")
+  api("io.github.portfoligno.micrixalus", "scala_$v", "1.1.0")
+  api("io.github.portfoligno.micrixalus", "scala-predef_$v", "1.1.0")
 
   implementation("org.http4s", "http4s-blaze-server_$v", "1.0.0-M20")
   implementation("org.http4s", "http4s-blaze-client_$v", "1.0.0-M20")
